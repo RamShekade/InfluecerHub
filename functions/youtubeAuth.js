6a888c4e43e33733943967889f24088431342a1c
RamShekade <ramshekade20@gmail.com>
2025-01-02 15:45:39 +0530

    new

--- functions/youtubeAuth.js
+++ functions/youtubeAuth.js
@@ -1,9 +1,11 @@
 const { google } = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
-
+require('dotenv').config();
 // Load your OAuth 2.0 client ID and client secret from the environment or config
-const CLIENT_ID = '464951632568-2nca3msgickb1p6e8sb1mq549q518c8n.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
+const CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID;
+const CLIENT_SECRET = process.env.GOOGLE_OAUTH_CLIENT_SECRET;
+
+
 const REDIRECT_URI = 'http://localhost:8080/user/oauth2callback';  // e.g., http://localhost:3000/oauth2callback
 
 const oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
